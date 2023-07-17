b12a5e58e1a4b7bfdfda7687f74f2f22c4a88827
kanabtilahun <kanabyatilahun@gmail.com>
2023-07-17 12:09:44 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.n>
+/**
+ * main -program that print statement followed with new line whith put function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
